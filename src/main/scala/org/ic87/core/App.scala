3a01bb608519e0cb8d16a2a016438e4a736159e8
Thomas Rawyler <rawyler@puzzle.ch>
2009-08-31 18:13:30 +0200

    added initial maven pom

---
+++ src/main/scala/org/ic87/core/App.scala
@@ -0,0 +1,9 @@
+package org.ic87.core
+
+/**
+ * Hello world!
+ *
+ */
+object App extends Application {
+  println( "Hello World!" )
+}
---
+++ src/test/scala/org/ic87/core/AppTest.scala
@@ -0,0 +1,17 @@
+package org.ic87.core
+
+import org.junit._
+import Assert._
+
+@Test
+class AppTest {
+
+    @Test
+    def testOK() = assertTrue(true)
+
+//    @Test
+//    def testKO() = assertTrue(false)
+
+}
+
+
